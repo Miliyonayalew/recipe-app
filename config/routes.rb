e89be3cc75d74f17763ab578742b560ad5370772
IngDavidTM <david-1011@hotmail.com>
2023-02-06 16:10:03 -0500

    Add devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: 'users#index'
   # root "articles#index"
 end
