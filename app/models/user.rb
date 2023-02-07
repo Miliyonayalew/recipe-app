f179427da889b38592b20fdcd8e933c2bf11ec05
IngDavidTM <david-1011@hotmail.com>
2023-02-07 15:55:38 -0500

    Fix errors from rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   def is?(requested_role)
     role == requested_role.to_s
   end
-  
+
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
