b129ff57ff2b168a48870d1010fbdb887ad0e0c5
IngDavidTM <david-1011@hotmail.com>
2023-02-08 16:42:11 -0500

    Modify the controller to show all ingredients from the current user

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -1,25 +1,22 @@
 class GeneralShoppingListController < ApplicationController
-  before_action :set_recipe, only: [:index]
-
   def index
-    @recipe_foods = @recipe.recipe_foods.includes(:food)
-
-    @missing_ingredients = @recipe_foods.select { |recipe_food| recipe_food.quantity > recipe_food.food.quantity }
-    @missing_ingredients = @missing_ingredients.map do |recipe_food|
+    @foods = Food.includes(:recipe_foods).where(user_id: current_user.id)
+    @missing_ingredients = []
+    @foods.each do |food|
+      total_quantity = 0
+      food.recipe_foods.each do |recipe_food|
+        total_quantity += recipe_food.quantity
+      end
+      @missing_ingredients << food if total_quantity > food.quantity
+    end
+    @missing_ingredients = @missing_ingredients.map do |food|
       {
-        name: recipe_food.food.name,
-        unit: recipe_food.food.measurement_unit,
-        quantity: recipe_food.quantity - recipe_food.food.quantity,
-        price: recipe_food.food.price * (recipe_food.quantity - recipe_food.food.quantity)
+        name: food.name,
+        unit: food.measurement_unit,
+        quantity: food.recipe_foods.sum(:quantity) - food.quantity,
+        price: food.price * (food.recipe_foods.sum(:quantity) - food.quantity)
       }
     end
-
     @total_price = @missing_ingredients.map { |missing_ingredient| missing_ingredient[:price] }.sum
-  end
-
-  private
-
-  def set_recipe
-    @recipe = current_user.recipes.find(params[:recipe_id])
   end
 end
