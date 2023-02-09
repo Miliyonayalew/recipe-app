bf3cffbb98a7cdfb78fddab76e6a1fb3f65c9f53
Miliyonayalew <miliayalew@gmail.com>
2023-02-09 14:44:41 +0300

    Remove uncessary codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  ROLES = %i[admin default].freeze
-
-  def is?(requested_role)
-    role == requested_role.to_s
-  end
-
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
