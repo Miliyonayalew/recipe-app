a376a9d7bc0a4f98b88866028af109687d6a4aa4
Miliyonayalew <miliayalew@gmail.com>
2023-02-09 20:12:41 +0300

    Add validation for measurement unit

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,7 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
 
   validates :name, presence: true
-  validates :measurement_unit, presence: true
+  validates :measurement_unit, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
