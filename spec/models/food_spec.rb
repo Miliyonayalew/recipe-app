62d5db8697fea46c2beb44d07f014b9378a78d5a
Miliyonayalew <miliayalew@gmail.com>
2023-02-09 15:12:09 +0300

    Add association test for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -51,4 +51,15 @@ RSpec.describe Food, type: :model do
     end
   end
 
+  context 'Testing associations' do
+    it 'Food should belong to a user' do
+      @food = Food.reflect_on_association(:user)
+      expect(@food.macro).to eq(:belongs_to)
+    end
+
+    it 'Food should have many recipe items' do
+      @food = Food.reflect_on_association(:recipe_foods)
+      expect(@food.macro).to eq(:has_many)
+    end
+  end
 end
